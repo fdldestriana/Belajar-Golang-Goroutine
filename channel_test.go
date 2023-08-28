d971553c86b1ddbe61e95f460682a0c99d0048cf
fadlydestriana <fadlydestriana@gmail.com>
2023-08-29 05:47:25 +0700

    new commit

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -57,16 +58,66 @@ func TestChannelAsParameter(t *testing.T) {
 func OnlyIn(channel chan<- string) {
 	time.Sleep(2 * time.Second)
 	channel <- "Eko Kurniawan Khannedy"
+	channel <- "Fadly Destriana Rusmana"
+	// channel <- "Destriana Fadly Rusmana"
+	// channel <- "Rusmana Fadly Destriana "
+	// channel <- "Rusmana Destriana Fadly"
+	// channel <- "Fadly Rusmana Destriana"
 }
 func OnlyOut(channel <-chan string) {
 	data := <-channel
-	fmt.Println("Ini adalah nilai dari variable data dari function OnlyOut", data)
+	fmt.Println("Ini adalah data pertama dari function OnlyOut", data)
+	data2 := <-channel
+	fmt.Println("Ini adalah data kedua dari function OnlyOut", data2)
 }
 
 func TestInOutChannel(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
-	go OnlyOut(channel)
 	go OnlyIn(channel)
-	time.Sleep(2 * time.Second)
+	go OnlyOut(channel)
+	time.Sleep(5 * time.Second)
+}
+
+// range channel
+// mengirim dan mengambil data ke dan dari channel menggunakan for loop
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke" + strconv.Itoa(i)
+		}
+		close(channel) // jika tidak close maka akan terkena deadlock
+	}()
+	for data := range channel {
+		fmt.Println("Menerima data", data)
+	}
+}
+
+// select channel
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel pertama", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel kedua", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+
 }
